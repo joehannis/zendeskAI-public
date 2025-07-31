a6a903b22f9d227b8c77f3418f0fa4d93082112e
Joe Hannis <45033532+joehannis@users.noreply.github.com>
2025-07-31 16:45:29 +0100

    Delete api directory

--- api/routes/zendeskAIRoute.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import zendeskAIController from '../controllers/zendeskAIController.mjs';
-
-router.get('/', zendeskAIController);
-
-export default router;
